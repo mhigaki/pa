b382b61c6c832b26715bb02467073cccfde75f35
mhigaki <milton.higaki@gmail.com>
2020-04-20 19:25:41 -0300

    news

--- js/main.js
+++ js/main.js
@@ -139,6 +139,50 @@
         pro_sing_slider.trigger('prev.owl.carousel');
     });
 
+    /*
+    Sand newsletter
+    **********************************************************************/
+    $('#subscribe').click(function () {
+        var error = false;
+        var emailCompare = /^([a-z0-9_.-]+)@([0-9a-z.-]+).([a-z.]{2,6})$/; // Syntax to compare against input
+        var email = $('input#nlmail').val().toLowerCase(); // get the value of the input field
+        if (email == "" || email == " " || !emailCompare.test(email)) {
+            $('#err-subscribe').show(500);
+            $('#err-subscribe').delay(4000);
+            $('#err-subscribe').animate({
+                height: 'toggle'
+            }, 500, function () {
+                // Animation complete.
+            });
+            error = true; // change the error state to true
+        }
+
+        if (error === false) {
+            $.ajax({
+                type: 'POST',
+                url: 'newsletter.php',
+
+                data: {
+                    email: $('#nlmail').val()
+                },
+                error: function (request, error) {
+                    alert("An error occurred");
+                },
+                success: function (response) {
+                    if (response == 'OK') {
+                        $('#success-subscribe').show();
+                        $('#nlmail').val('')
+                    } else {
+                        alert("An error occurred");
+                    }
+                }
+            });
+        }
+
+        return false;
+    });
+
+    /*
     // --------- Google Map init ----------  //
     // var googleMapSelector = $('#map'),
     //     myCenter = new google.maps.LatLng(-46.620730, -23.602121);
