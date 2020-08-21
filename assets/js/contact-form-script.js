448d4eacc4e3cf3ee18c3848dbb9f45afd1d0976
Milton <milton.higaki@gmail.com>
2020-08-21 02:48:02 -0300

    form

--- assets/js/contact-form-script.js
+++ assets/js/contact-form-script.js
@@ -23,7 +23,7 @@ function submitForm() {
   $.ajax({
     type: "POST",
     url: "php/form-process.php",
-    data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
+    data: "name=" + name + "&email=" + email + "&cel_tel=" + cel_tel + "&msg_subject=" + msg_subject + "&message=" + message,
     success: function(text) {
       if (text == "success") {
         formSuccess();
